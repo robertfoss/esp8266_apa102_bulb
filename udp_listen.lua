85728443d0e1ca211d59bd86ef8851625afd2f79
Robert Foss <dev@robertfoss.se>
2016-03-05 15:44:16 -0500

    Added first version of v4 hardware.

--- udp_listen.lua
+++ udp_listen.lua
@@ -1,7 +1,10 @@
 function clear()
   local clearOne = string.char(0, 0, 0, 0)
   local clearAll = string.rep(clearOne, 100)
-  apa102.write(6, 5, clearAll)
+  apa102.write(6, 7, clearAll)
+  apa102.write(3, 5, clearAll)
+  apa102.write(1, 2, clearAll)
+  apa102.write(4, 8, clearAll)
 end
 
 function timeoutClear()
@@ -9,7 +12,10 @@ function timeoutClear()
 end
 
 function listen(s, data)
-  apa102.write(6, 5, data)
+  apa102.write(6, 7, data)
+  apa102.write(3, 5, data)
+  apa102.write(1, 2, data)
+  apa102.write(4, 8, data)
   timeoutClear()
 end
 
@@ -17,3 +23,6 @@ end
 sock=net.createServer(net.UDP) 
 sock:on("receive", listen)
 sock:listen(UDP_LISTEN_PORT)
+
+gpio.mode(0, gpio.OUTPUT)
+gpio.mode(5, gpio.OUTPUT)
